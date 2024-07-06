52916449cc1c108572546a9dfdd54b3e8a01b029
AdvaithGS <advaith.gs4@gmail.com>
2024-07-07 00:06:09 +0530

    modified libmfs.h

--- filesystems-distributed-ufs/libmfs.c
+++ filesystems-distributed-ufs/libmfs.c
@@ -1,5 +1,5 @@
 #include "ufs.h"
-#include "udp.h"
+#include "udp.c"
 #include "mfs.h"
 #include <assert.h>
 #include <fcntl.h>
@@ -20,29 +20,38 @@ struct sockaddr_in addrSnd, addrRcv;
 int sd;
 int rc;
 
+int sendMessage(mes_t * message){
+  printf("client:: send message type [%d]\n", message->mes_type);
+  rc = UDP_Write(sd, &addrSnd, (char *)message, sizeof(mes_t));
+  if (rc < 0){
+    printf("ERROR : CANNOT COMMMUNICATE TO SERVER\n");
+  }else{
+    printf("Message sent\n");
+  }
+  res_t * response;
+  rc = UDP_Read(sd, &addrRcv, (char *)response, BUFFER_SIZE);
+  if(response->code == 1){
+    printf("Reponse received: {%s}",response->content);
+  }else{
+    printf("REQUEST ERROR: {%s}",response->content);
+  }
+  return 1;
+}
+
+
 int makeConnection(char *hostname, int port){
   struct sockaddr_in addrSnd, addrRcv;
   sd = UDP_Open(20000);
   rc = UDP_FillSockAddr(&addrSnd, hostname, port);
+  assert(sd>0 && rc>=0);
+  mes_t sendt;
+  sendt.mes_type = __wave;
+  sendMessage(&sendt);
   connected = true;
+  printf("Made Connection\n");
   return 0;
 }
 
-int sendMessage(mes_t * message){
-  printf("client:: send message [%d]\n", message->type);
-  rc = UDP_Write(sd, &addrSnd, (char *)message, BUFFER_SIZE);
-  if (rc < 0){
-    return 0;
-  }
-  res_t * response;
-  rc = UDP_Read(sd, &addrRcv, response, BUFFER_SIZE);
-  if(response->code == 1){
-    printf("Reponse received: {%s}",response->content);
-  }else{
-    return 0;
-  }
-  return 1;
-}
 
 int MFS_Init(char *hostname, int port){
   if (connected){
@@ -55,8 +64,15 @@ int MFS_Init(char *hostname, int port){
   return 0;
 }
 int MFS_Lookup(int pinum, char *name){
-  
+  mes_t message;
+  message.type = __lookup;
+  message.pinum = pinum;
+  message.name = name;
+  printf("On lookup now\n");
+  sendMessage(&message);
+  return 1;
 };
+
 int MFS_Stat(int inum, MFS_Stat_t *m);
 int MFS_Write(int inum, char *buffer, int offset, int nbytes);
 int MFS_Read(int inum, char *buffer, int offset, int nbytes);
@@ -64,16 +80,9 @@ int MFS_Creat(int pinum, int type, char *name);
 int MFS_Unlink(int pinum, char *name);
 int MFS_Shutdown(){
   mes_t * mes;
-  mes->mes_type = shut;
-
+  mes->mes_type = __shut;
   exit(1);
 };
-
-int main(int argc, char **argv)
-{
-  super_t *s = (super_t *)image_data(argv[1]);
-  printf("%d\n", s->inode_bitmap_addr);
-}
 
 // total blocks        36
 // inodes            32 [size of each: 128]
